2b2a3f6492f7c4ed6b4513dc7373034ed3830fb4
Erick Santos <erickosantos.dev@gmail.com>
2021-10-18 19:57:43 -0300

    creating login and callback route from github

--- node/src/App.ts
+++ node/src/App.ts
@@ -1,5 +1,16 @@
+import 'dotenv/config';
 import Express from 'express';
 
 const App = Express();
+
+App.get('/github', (request, response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+});
+
+App.get('/signin/callback', (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
 
 App.listen(4000, () => console.log(`Server is running on PORT 4000`));
